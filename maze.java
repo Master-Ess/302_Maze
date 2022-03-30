cc3f97b7abeaae3546631909e31cc20470d59ce6
Master-Ess <67045069+Master-Ess@users.noreply.github.com>
2022-03-30 18:24:46 +1000

    GUI

---
+++ maze.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class maze{
+
+    private static void createWindow() {
+        JFrame frame = new JFrame("mELK");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel textLabel = new JLabel("Im contents or something", SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args){
+        createWindow();
+    }
+
+
+}
