8284fdaa784d56b94dfe620111b06657abe95c1e
Elliot Gregory <elliot.gregory.work@gmail.com>
2022-04-28 23:27:57 +1000

    1.18 Fixing bug and merge error
    Adjusting alignment of the block output

--- src/util/MazeBlocks.java
+++ src/util/MazeBlocks.java
@@ -19,6 +19,7 @@ public class MazeBlocks extends JPanel{
 	
 	@Override
 	protected void paintComponent(Graphics g) {
+		int blockNum = data.getWidth() * 2 + 1;
 		super.paintComponent(g);
         //In each of these commented spots, add the builder for a block
     	//y is the row number, x is the column number
@@ -26,25 +27,25 @@ public class MazeBlocks extends JPanel{
         for(int y = 0; y < data.getHeight(); y++){
         	//Top row
             for(int x = 0; x < data.getWidth(); x++){
-            	drawHoriBlock(g, x * length + padding, y * length + padding);
+            	drawHoriBlock(g, x * length + padding, y * length + padding, data.getBlockState(y * blockNum + x));
             }
             //Vertical Lines
             for(int x = data.getWidth(); x < data.getWidth() * 2 + 1; x++){
-                drawVertBlock(g, x * length - length + padding, y * thickness + padding);
+                drawVertBlock(g, x * length + padding, y * length + padding, data.getBlockState(y * blockNum + x));
             }
         }
         //gets bottom line
         for(int x = 0; x < data.getWidth(); x++){
-        	drawHoriBlock(g, x * length + padding, data.getHeight() * length);
+        	drawHoriBlock(g, x * length + padding, data.getHeight() * length, data.getBlockState(data.getHeight() * blockNum + x));
         }
 	}
 	
-	private void drawVertBlock(Graphics g, int x, int y){
+	private void drawVertBlock(Graphics g, int x, int y, boolean on){
 		g.drawRect(x, y, thickness, length + thickness);
 		g.fillRect(x, y, thickness, length + thickness);
 	}
 	
-	private void drawHoriBlock(Graphics g, int x, int y){
+	private void drawHoriBlock(Graphics g, int x, int y, boolean on){
 		g.drawRect(x, y, length + thickness, thickness);
 		g.fillRect(x, y, length + thickness, thickness);
 	}
